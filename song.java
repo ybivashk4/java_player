4d790bb845de633a5a7d3f0250b9ccf7ad15e8f8
AdminLinux <ybivashk4>
2024-04-01 14:29:04 +0500

    za paru

--- song.java
+++ song.java
@@ -1,28 +1,33 @@
 package java_player;
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.util.Scanner;
 
 public class song {
-    private final String name;
-    private final String artist;
-    private final long duration;
+    private String name;
+    private String artist;
+    private long duration;
     public song (String path) {
         try (Scanner scanner = new Scanner(new File(path))) {
-            int i=0;
             name = scanner.nextLine();
             artist = scanner.nextLine();
             duration = Long.parseLong(scanner.nextLine());
         }
         catch (FileNotFoundException e) {
-            System.out.println(path + " not found");
+            System.out.println(path + " not found" + this.getClass());
             /*name = "";
             artist = "";
             duration = -1;*/
         }
+    }
+    public String get_name() {return name;}
+    public String get_artist() {return artist;}
+    public long get_duration() {return duration;}
+    public void out() {
+        System.out.println(name);
+        System.out.println(artist);
+        System.out.println(duration);
     }
 
 }
